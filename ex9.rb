864b58728dcbf74d774d20b68cff489b3b91ab18
JC <node.zerocool@gmail.com>
2020-10-13 22:09:05 -0500

    new line

--- ex9.rb
+++ ex9.rb
@@ -1,5 +1,5 @@
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}"
