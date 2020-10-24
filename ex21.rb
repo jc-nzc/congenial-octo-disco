ee756c314e5105a3cef1db2cc501a4dadcad55f2
JC <node.zerocool@gmail.com>
2020-10-23 21:47:10 -0500

    functions

---
+++ ex21.rb
@@ -0,0 +1,14 @@
+def add(a, b)
+  puts "Adding #{a} + #{b}"
+  return a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  return a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  return a * b
+end
