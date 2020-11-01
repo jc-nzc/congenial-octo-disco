f8e76429fa5f2aed1e3b64bdaa93b758237b7bed
JC <node.zerocool@gmail.com>
2020-10-31 20:06:09 -0500

    poems

--- ex24.rb
+++ ex24.rb
@@ -1,2 +1,16 @@
 puts "Let's practice everything."
-puts 'You\'d need to know \' bout escapes with \\ that do \n newlines and \t tabs.'
+puts 'You\'d need to know \' bout escapes with \\ that do \n newlines and \t tabs.'\
+
+# the <<END is a "heredoc". See the Student Questions.
+poem = <<END
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+END
+
+puts "--------------"
+puts poem
+puts "--------------"
