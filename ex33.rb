5a32632b5a5264b0f1453238c08df6a603175e4f
JC <node.zerocool@gmail.com>
2020-11-09 22:23:04 -0600

    while loops

---
+++ ex33.rb
@@ -0,0 +1,18 @@
+i = 0
+number = []
+
+while i < 6
+  puts "At the top i is #{i}"
+  numbers.push(i)
+
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is ${i}"
+end
+
+puts "The numbers: "
+
+# remember you can write this 2 other ways?
+numbers.each {|num| puts num }
+
+
