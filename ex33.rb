be44b5c67bfdafd5cb544d54c071c8d8a1015008
JC <node.zerocool@gmail.com>
2020-11-13 22:08:49 -0600

    basics

--- ex33.rb
+++ ex33.rb
@@ -1,20 +1,28 @@
-i = 0
-numbers = []
+# Convert this while-loop to a function that you can call, and replace 6 in the test (i < 6) with a variable.
+# Use this function to rewrite the script to try different numbers.
+# def func_test(x)
+#   i = 0
+#   numbers = []
+#   while i < 6
+#     puts "At the top i is #{i}"
+#     numbers.push(i)
+#
+#     i += 1
+#     puts "Numbers now: ", numbers
+#     puts "At the bottom i is #{i}"
+#   end
+# end
+#
+# puts "The numbers: "
+#
+# # remember you can write this 2 other ways?
+# numbers.each {|num| puts num }
+#
+# func_test(x)
 
 
-# Function Example
-def custom_func(cheese_count, boxes_of_crackers)
-  while i < 6
-    puts "At the top i is #{i}"
-    numbers.push(i)
-
-    i += 1
-    puts "Numbers now: ", numbers
-    puts "At the bottom i is #{i}"
-  end
+def doubler(start)
+  puts start * 2
 end
 
-puts "The numbers: "
-
-# remember you can write this 2 other ways?
-numbers.each {|num| puts num }
+doubler(5)
