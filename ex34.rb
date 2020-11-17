1816fe784c6d224c1df8b480c51800b686f5cffc
JC <node.zerocool@gmail.com>
2020-11-16 22:58:26 -0600

    loops and arrays exc

--- ex34.rb
+++ ex34.rb
@@ -1,9 +1,25 @@
-animals = ['bear', 'tiger', 'penguin', 'zebra']
-bear = animals[0]
-liger = animals[1]
-puts bear
-puts liger
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+# bear = animals[0]
+# liger = animals[1]
+# puts bear
+# puts liger
 
 for i in animals
   puts i
 end
+
+# puts "that's one way"
+# puts "or this way"
+
+animals.each do |animal|
+  puts "I present to you the #{animal}"
+  sleep(2)
+end
+
+puts "or this way"
+animals.each {|i| puts "here's a #{i}"}
+# elements = []
+# (0..4).each do |i|
+#   puts "adding #{i} the list"
+#   elements.push(i)
+# end
