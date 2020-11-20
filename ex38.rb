d67d7e869148241ac1ed514887e0e1511c40fd8f
JC <node.zerocool@gmail.com>
2020-11-19 23:03:16 -0600

    arrays

---
+++ ex38.rb
@@ -0,0 +1,6 @@
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+puts "Wait there are not 10 things in that list. Let's fix that."
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
