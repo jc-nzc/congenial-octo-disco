36522fa9c91120544e7538b242e35c117c63e989
JC <node.zerocool@gmail.com>
2020-11-26 00:43:15 -0600

    missing array

--- ex40.rb
+++ ex40.rb
@@ -10,5 +10,8 @@ class Song
 end
 
 happy_bday = Song.new(["Happy Birthday to you", "I don't want you to get sued", "So I'll stop right there"])
+bulls_on_parade = Song.new(["They rally around tha family",
+            "With pockets full of shells"])
+
 happy_bday.sing_me_a_song
 bulls_on_parade.sing_me_a_song
