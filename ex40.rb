85b5b658d44e926b7fe2950486ddbe65e3416946
JC <node.zerocool@gmail.com>
2020-11-26 00:40:55 -0600

    classes modules

---
+++ ex40.rb
@@ -0,0 +1,13 @@
+class Song
+
+  def initialize(lyrics)
+    @lyrics = lyrics
+  end
+
+  def sing_me_a_song()
+    @lyrics.each {|line| puts line}
+  end
+end
+
+happy_bday = Song.new(["Happy Birthday to you", "I don't want you to get sued", "So I'll stop right there"])
+happy_bday.sing_me_a_song
