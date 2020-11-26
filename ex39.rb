2d69d734a10edf1ca9412b093d089cda81390259
JC <node.zerocool@gmail.com>
2020-11-25 18:19:04 -0600

    cities

--- ex39.rb
+++ ex39.rb
@@ -1,1 +1,19 @@
 # creating a hash map
+states = {
+  'Texas' => 'TX',
+  'Nebraska' => 'NE',
+  'California' => 'CA',
+  'New York' => 'NY',
+  'Arizona' => 'AZ'
+}
+
+# create a basic set of states and some cities in them
+cities = {
+  'CA' => 'San Francisco',
+  'AZ' => 'Tempe',
+  'TX' => 'Austin'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['NE'] = 'Omaha'
