0bf3f6576ada812bc088ffdc084a868116cfe946
JC <node.zerocool@gmail.com>
2020-11-30 23:36:53 -0600

    scene

--- ex43.rb
+++ ex43.rb
@@ -4,3 +4,23 @@ class Scene
     exit(1)
   end
 end
+
+class Engine
+
+  def initialize(scene_map)
+    @scene_map = scene_map
+  end
+
+  def play()
+    current_scene = @scene_map.opening_scene()
+    last_scene = @scene_map.next_scene('finished')
+
+    while current_scene != last_scene
+      next_scene_name = current_scene.enter()
+      current_scene = @scene_map.next_scene(next_scene_name)
+    end
+
+    # be sure to print out the last scene
+    current_scene.enter()
+  end
+end
