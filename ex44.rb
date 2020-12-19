81d896d18154ea3cd6c8409f7dc79077fc7ddd72
JC <node.zerocool@gmail.com>
2020-12-19 09:32:24 -0600

    override

--- ex44.rb
+++ ex44.rb
@@ -14,21 +14,42 @@
 # dad.implicit()
 # son.implicit()
 
-class Parent
+# class Parent
+#
+#   def override()
+#     puts "PARENT override()"
+#   end
+# end
+#
+# class Child < Parent
+#   def override()
+#     puts "CHILD override()"
+#   end
+# end
+#
+# dad = Parent.new()
+# son = Child.new()
+#
+# dad.override()
+# son.override()
 
-  def override()
-    puts "PARENT override()"
+class Parent
+  def altered()
+    puts "PARENT altered()"
   end
 end
 
 class Child < Parent
-  def override()
-    puts "CHILD override()"
+  def altered()
+    puts "CHILD, BEFORE PARENT altered()"
+    super()
+    puts "CHILD, AFTER PARENT altered()"
   end
+
 end
 
 dad = Parent.new()
 son = Child.new()
 
-dad.override()
-son.override()
+dad.altered()
+son.altered()
