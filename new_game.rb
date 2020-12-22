92140ce429e1cf6a335ff52f8d1e15ef1bc0daab
JC <node.zerocool@gmail.com>
2020-12-21 22:09:45 -0600

    game basics

---
+++ new_game.rb
@@ -0,0 +1,28 @@
+# instructions
+
+# Here are your requirements:
+
+# Make a different game from the one I made.
+# Use more than one file, and use require to use them. Make sure you know what that is.
+# Use one class per room and give the classes names that fit their purpose (like GoldRoom, KoiPondRoom).
+# Your runner will need to know about these rooms, so make a class that runs them and knows about them. There's plenty of ways to do this, but consider having each room return what room is next or setting a variable of what room is next.
+# Other than that I leave it to you. Spend a whole week on this and make it the best game you can. Use classes, functions, dicts, arrays, anything you can to make it nice. The purpose of this lesson is to teach you how to structure classes that need other classes inside other files.
+
+```
+Rooms = ["Garden of Life", "Spaceship Expander Port", "Sandbox Sunset", "Endless Forrest"]
+
+class GardenOfLifeRoom
+  #something
+end
+
+class SpaceshipExpanderPortRoom
+ #something
+end
+
+class SandboxSunsetRoom
+ #something
+end
+
+class EndlessForrestRoom
+ #something
+end
