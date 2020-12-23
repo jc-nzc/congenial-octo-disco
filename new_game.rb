6bf38e5775a0bede9eedf47b7d1240d029d458ae
JC <node.zerocool@gmail.com>
2020-12-22 19:34:06 -0600

    class names, functions

--- new_game.rb
+++ new_game.rb
@@ -8,21 +8,29 @@
 # Your runner will need to know about these rooms, so make a class that runs them and knows about them. There's plenty of ways to do this, but consider having each room return what room is next or setting a variable of what room is next.
 # Other than that I leave it to you. Spend a whole week on this and make it the best game you can. Use classes, functions, dicts, arrays, anything you can to make it nice. The purpose of this lesson is to teach you how to structure classes that need other classes inside other files.
 
-```
-Rooms = ["Garden of Life", "Spaceship Expander Port", "Sandbox Sunset", "Endless Forrest"]
+rooms = ["Garden of Life", "Spaceship Expander Port", "Sandbox Sunset", "Endless Forrest"]
+puts rooms
 
 class GardenOfLifeRoom
-  #something
+  #somethingz
+  def()
+  puts "You've entered the garden of eden"
 end
 
 class SpaceshipExpanderPortRoom
  #something
+ def()
+ puts "You've parked your space explorer"
 end
 
 class SandboxSunsetRoom
  #something
+ def()
+ puts "Push it to the limits"
 end
 
 class EndlessForrestRoom
  #something
+ def()
+ puts "Plant a tree for the future, meditate"
 end
