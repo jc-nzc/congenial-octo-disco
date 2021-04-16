c061bdc0bbd2031471ba7dd64dff0a77578b3960
Zero <node.zerocool@gmail.com>
2021-04-15 23:28:19 -0500

    puts ex

--- singleton.rb
+++ singleton.rb
@@ -1,6 +1,8 @@
 my_lambda = ->(a, b)  { a + b }
 my_proc   = Proc.new  { |a, b| a + b }
-my_lambda.call(2)
+
+puts my_lambda.call(2,1)
 # ArgumentError: wrong number of arguments (1 for 2)
-my_proc.call(2)
+
+puts my_proc.call(2,8)
 # TypeError: nil can't be coerced into Fixnum
